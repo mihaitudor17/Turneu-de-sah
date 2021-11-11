4b3d938a6be0ea1f511ef1fb307b26aeca611255
Nutzu000 <glizard.glizard@yahoo.com>
2021-11-11 10:46:03 +0200

    Switched to spring boot

---
+++ TurneuDeSah/src/test/java/com/example/demo/person/Person.java
@@ -0,0 +1,13 @@
+package com.example.demo.person;
+import java.util.Date;
+
+public class Person {
+    private int personID;
+    private String lastName; //nume de familie
+    private String name;
+    private String cnp;
+    private Date dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private Boolean isActive;
+}
