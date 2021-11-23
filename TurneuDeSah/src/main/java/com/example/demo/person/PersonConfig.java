831c11b0c77c0465a1d37708dfeb081dab3a4d27
Nutzu000 <glizard.glizard@yahoo.com>
2021-11-23 16:54:21 +0200

    The data remains in the database even after closing the server application.

--- TurneuDeSah/src/main/java/com/example/demo/person/PersonConfig.java
+++ TurneuDeSah/src/main/java/com/example/demo/person/PersonConfig.java
@@ -9,30 +9,30 @@ import java.util.List;
 
 @Configuration
 public class PersonConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(PersonRepository repository) {
-        return args -> {
-            Person p1 = new Person(
-                    "Blaga",
-                    "Mihai",
-                    "5010514080001",
-                    LocalDate.of(2001, 05, 01),
-                    "Barbat",
-                    "0751515151",
-                    true
-            );
-            Person p2 = new Person(
-                    "Antal",
-                    "Eduard",
-                    "5010514080002",
-                    LocalDate.of(2001, 9, 02),
-                    "Barbat",
-                    "0751515152",
-                    true
-            );
-            repository.saveAll(
-                    List.of(p1, p2)
-            );
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(PersonRepository repository) {
+//        return args -> {
+//            Person p1 = new Person(
+//                    "Blaga",
+//                    "Mihai",
+//                    "5010514080001",
+//                    LocalDate.of(2001, 05, 01),
+//                    "Barbat",
+//                    "0751515151",
+//                    true
+//            );
+//            Person p2 = new Person(
+//                    "Antal",
+//                    "Eduard",
+//                    "5010514080002",
+//                    LocalDate.of(2001, 9, 02),
+//                    "Barbat",
+//                    "0751515152",
+//                    true
+//            );
+//            repository.saveAll(
+//                    List.of(p1, p2)
+//            );
+//        };
+//    }
 }
