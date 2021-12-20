059542c1543434a0a75adafc9e576f5b8dc11df8
Nutzu000 <glizard.glizard@yahoo.com>
2021-12-20 12:43:46 +0200

    Users can now register

---
+++ TurneuDeSah/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controllers;
+
+import com.example.demo.classes.Person;
+import com.example.demo.classes.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/loginUser", method= RequestMethod.POST)
+    public String login(@ModelAttribute User user) {
+
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "/saveUser", method= RequestMethod.POST)
+    public String save(@ModelAttribute User user) {
+        System.out.println(user.toString());
+        User newUser=new User();
+        System.out.println(newUser.toString());
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(user.getPassword());
+        newUser.setAdmin(false);
+        userService.addNewUser(newUser);
+        return "redirect:";
+    }
+}
